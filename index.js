f3b9b27ac482c793f879a975ee0ec44cf84b7580
JurgenLB <77586573+JurgenLB@users.noreply.github.com>
2025-09-27 17:26:51 +0200

    Refactor PhilipsTV accessory setup and features

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ class PhilipsTvAccessory {
         Characteristic = api.hap.Characteristic;
         Categories = api.hap.Categories;
 
-        this.PhilipsTV = new PhilipsTV(config, Service, Characteristic);
+        this.on = false;
+        this.volume = 0;
+
+        this.inputSources = [];
+        this.ambilightModes = ["FOLLOW_VIDEO", "FOLLOW_AUDIO", "Lounge_light"];
+        this.activeAmbilightMode = 0;
+        this.PhilipsTV = new PhilipsTV(config);
 
         this.api.on("didFinishLaunching", () => {
             this.publishExternal();
         });
-    }
-
-    getServices() {
-        return this.PhilipsTV.getServices();
     }
 
     publishExternal() {
@@ -33,15 +35,89 @@ class PhilipsTvAccessory {
         this.tvAccessory = new this.api.platformAccessory(this.config.name, uuid, Categories.TELEVISION);
         this.tvAccessory.context.isexternal = true;
 
-        // Get all services from PhilipsTV
-        const services = this.PhilipsTV.getServices();
+        this.tvService = new Service.Television(this.config.name);
+        this.tvService.setCharacteristic(Characteristic.ConfiguredName, this.config.name);
+        this.tvService.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
 
-        // Add all services to the accessory
-        services.forEach(service => {
-            this.tvAccessory.addService(service);
-        });
+        this.tvService.getCharacteristic(Characteristic.Active)
+            .onGet(() => this.PhilipsTV.getPowerState())
+            .onSet((v) => this.PhilipsTV.setPowerState(v));
+
+        this.tvService.getCharacteristic(Characteristic.RemoteKey)
+            .onSet((v) => this.PhilipsTV.sendRemoteKey(v));
+
+        this.tvSpeaker = new Service.TelevisionSpeaker(this.config.name + " Speaker");
+        this.tvSpeaker.setCharacteristic(Characteristic.VolumeControlType, Characteristic.VolumeControlType.ABSOLUTE);
+        this.tvSpeaker.getCharacteristic(Characteristic.Volume)
+            .onGet(() => this.PhilipsTV.getVolumeState())
+            .onSet((v) => this.PhilipsTV.setVolumeState(v));
+        this.tvSpeaker.getCharacteristic(Characteristic.Mute)
+            .onGet(() => this.PhilipsTV.getMuteState())
+            .onSet((v) => this.PhilipsTV.setMuteState(v));
+
+        this.tvService.addLinkedService(this.tvSpeaker);
+
+        this.setupInputSources();
+        this.setupAmbilightInputs();
+
+        this.informationService = new Service.AccessoryInformation()
+            .setCharacteristic(Characteristic.Name, this.config.name)
+            .setCharacteristic(Characteristic.Manufacturer, 'Philips')
+            .setCharacteristic(Characteristic.Model, 'Android TV')
+            .setCharacteristic(Characteristic.SerialNumber, 'PhilipsTV-' + this.config.name)
+            .setCharacteristic(Characteristic.FirmwareRevision, pkg.version);
+
+        this.tvAccessory.addService(this.informationService);
+        this.tvAccessory.addService(this.tvService);
+        this.tvAccessory.addService(this.tvSpeaker);
 
         this.api.publishExternalAccessories(pluginName, [this.tvAccessory]);
+    }
+
+    setupInputSources() {
+        if (!this.config.inputs) return;
+        this.tvService.setCharacteristic(Characteristic.ActiveIdentifier, 0);
+        this.tvService.getCharacteristic(Characteristic.ActiveIdentifier)
+            .onGet(() => 0)
+            .onSet(() => {});
+
+        this.config.inputs.forEach((input, index) => {
+            const inputSource = new Service.InputSource(input.name, input.name);
+            inputSource
+                .setCharacteristic(Characteristic.Identifier, index)
+                .setCharacteristic(Characteristic.ConfiguredName, input.name)
+                .setCharacteristic(Characteristic.IsConfigured, Characteristic.IsConfigured.CONFIGURED)
+                .setCharacteristic(Characteristic.InputSourceType, Characteristic.InputSourceType.APPLICATION)
+                .setCharacteristic(Characteristic.CurrentVisibilityState, Characteristic.CurrentVisibilityState.SHOWN);
+
+            this.tvAccessory.addService(inputSource);
+            this.tvService.addLinkedService(inputSource);
+        });
+        }
+
+    setupAmbilightInputs() {
+        const baseId = (this.config.inputs?.length || 0);
+        this.tvService.getCharacteristic(Characteristic.ActiveIdentifier)
+            .onSet(async (value) => {
+                if (value >= baseId && value < baseId + this.ambilightModes.length) {
+                    const mode = this.ambilightModes[value - baseId];
+                    await this.PhilipsTV.setAmbilightMode(mode);
+                }
+            });
+
+        this.ambilightModes.forEach((mode, idx) => {
+            const id = baseId + idx;
+            const inputSource = new Service.InputSource(mode, "Ambilight " + mode);
+            inputSource
+                .setCharacteristic(Characteristic.Identifier, id)
+                .setCharacteristic(Characteristic.ConfiguredName, "Ambilight " + mode)
+                .setCharacteristic(Characteristic.IsConfigured, Characteristic.IsConfigured.CONFIGURED)
+                .setCharacteristic(Characteristic.InputSourceType, Characteristic.InputSourceType.HDMI)
+                .setCharacteristic(Characteristic.CurrentVisibilityState, Characteristic.CurrentVisibilityState.SHOWN);
+
+            this.tvAccessory.addService(inputSource);
+            this.tvService.addLinkedService(inputSource);
+        });
     }
 }
 
